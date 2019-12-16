caf4ca7a695b7e1a96f0727ed1db04df87356422
kadiryolalan <kadiryolalan@gmail.com>
2019-12-16 11:53:32 +0300

    Final Validator class

--- src/Validator.php
+++ src/Validator.php
@@ -4,9 +4,8 @@ namespace Codes50;
 
 use Codes50\Core\CoreValidator;
 
-class Validator extends CoreValidator
+final class Validator extends CoreValidator
 {
-
     public function __construct(array $data = [], array $rules = [])
     {
         parent::__construct($data, $rules);
